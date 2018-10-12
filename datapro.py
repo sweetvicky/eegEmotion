e9964c56fcbd152db63e32f2eecf65cddc0ee178
sweety <sweet.wenjing@gmail.com>
2018-10-12 23:39:30 +0800

    change some file

--- datapro.py
+++ datapro.py
@@ -1,10 +1,102 @@
 import pickle
 import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt 
+from mpl_toolkits.mplot3d import Axes3D
 
-rootdir = '..//data'
-files=os.listdir(rootdir) #列出当前目录下所有的文件
-for filename in files:
-	rootdir1=rootdir+'/'+filename
+def savedatasets(dirname,dataname,labelname,eegchanel,eegnum,trigernum):
+	files = os.listdir(dirname)
+	subdrows = eegchanel * trigernum
+	drows = len(files) * subdrows
+	dcloums = eegnum
+	labelen = len(files) * trigernum
+	datas = np.zeros((drows,dcloums),dtype=np.float)
+	labels = np.zeros((labelen,4))
+	dnum = 0
+	lnum = 0
+	for filename in files:
+		filenametmp = dirname + '/' + filename
+		datatmp = pickle.load(open(filenametmp, 'rb'),encoding='iso-8859-1')
+		# datas[dnum:dnum+subdrows,:] = datatmp['data'][:,:eegchanel,-eegnum:].reshape((-1,eegnum))
+		labels[lnum:lnum+trigernum,:] = datatmp['labels'] 
+		dnum += subdrows
+		lnum += trigernum
 
-x = pickle.load(open(rootdir1, 'rb'),encoding='iso-8859-1')
-print(x['data'].shape)
+	return datas,labels 
+	
+
+def plotVAemotion(labels):
+	neglabels = labels[labels[:,0]<5,:]
+	poslabels = labels[labels[:,0]>5,:]
+	callabels = labels[labels[:,0]==5,:]
+
+	# 画二维图像
+	# plt.figure(facecolor='w',figsize=(9,8))
+	# plt.plot(neglabels[:,0],neglabels[:,1],'mv',label='消极')
+	# plt.plot(poslabels[:,0],poslabels[:,1],'ro',label='积极')
+	# plt.plot(callabels[:,0],callabels[:,1],'b*',label='平静')
+	# plt.grid()
+	# plt.title('Valence-Arousal维度标签')
+	# plt.show()
+
+	# 画三维图像
+	fig = plt.figure(figsize=(8,8))
+	ax = fig.add_subplot(111, projection='3d')
+	ax.scatter(neglabels[:,0], neglabels[:,1], neglabels[:,3],
+           marker='v', color='b', label='消极')
+	ax.scatter(poslabels[:,0], poslabels[:,1], poslabels[:,3],
+           marker='o', color='m', label='积极')
+	ax.scatter(callabels[:,0], callabels[:,1], callabels[:,3],
+           marker='^', color='r', label='平静')
+	ax.scatter(np.median(neglabels[:,0]), np.median(neglabels[:,1]), np.median(neglabels[:,3]),
+           marker='v', color='k', label='消极分类中心')
+	ax.scatter(np.median(poslabels[:,0]), np.median(poslabels[:,1]), np.median(poslabels[:,3]),
+           marker='o', color='k', label='消极分类中心')
+	plt.title('VAL情感维度标签')
+	ax.set_xlabel('Valence')
+	ax.set_ylabel('Arousal')
+	ax.set_zlabel('Liking')
+	plt.show()
+
+def labelsmapping(labels):
+	print(len(list(labels[:,0])))
+	newlabels = np.array(list(map(lambda x: int(x >= 4.5)+int(x > 5.5)-1, list(labels[:,0]))))
+	return newlabels
+
+if __name__ == '__main__':
+	eegchanel = 32
+	eegnum = 7680
+	trigernum = 40
+	rootdir = '..//data'
+	onerootdir = '..//onedata'
+	datas,labelss = savedatasets(onerootdir,'dataname','labelname',eegchanel,eegnum,trigernum)
+	# plotVAemotion(labelss)
+	labels = labelsmapping(labelss)
+	# print(datas.shape)
+	# print(labels.shape)
+	np.savetxt("onedatas.csv", datas, delimiter=',')
+	np.savetxt("onelabels.csv", labels, delimiter=',')
+
+	# testdata = np.array([5.4,6,8,2])
+	# newlabels = np.array(list(map(lambda x: int(x >= 4.5)+int(x > 5.5)-1, list(testdata))))
+	# print(newlabels)
+
+# testdata = np.loadtxt(open("datas.csv","rb"), delimiter=",", skiprows=0)
+# testlabel = np.loadtxt(open("labels.csv","rb"), delimiter=",", skiprows=0)
+# print(testdata.shape)
+# print(testlabel)
+
+# print('Valence的min：%.2f,max:%.2f,mean:%.2f。'%(min(labels[:,0]),max(labels[:,0]),np.mean(labels[:,0])))
+# print('Arousal的min：%.2f,max:%.2f'%(min(labels[:,1]),max(labels[:,1])))
+
+
+# files=os.listdir(rootdir) #列出当前目录下所有的文件
+# print(type(files))
+# for filename in files:
+# 	rootdir1=rootdir+'/'+filename
+
+# x = pickle.load(open(rootdir1, 'rb'),encoding='iso-8859-1')
+# y = x['data']
+# print(y.shape)
+# print(y[:,:eegchanel,-eegnums:].reshape((-1,eegnums)).shape)
--- train.py
+++ train.py
@@ -1,31 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.signal import butter, lfilter
 
 def loaddata(dataname,labelname):
 	data = np.loadtxt(open("datas.csv","rb"), delimiter=",", skiprows=0)
 	label = np.loadtxt(open("labels.csv","rb"), delimiter=",", skiprows=0)
 
-def getfft(rowdata):
-	fftdata = map(lambda x: np.fft.fft(x),list(rowdata))
-	return fftdata
+# def getfft(rowdata):
+# 	fftdata = map(lambda x: np.fft.fft(x),list(rowdata))
+# 	return fftdata
 
 def getfrequency(rowdata,fs):
 	# 每次输入一个样本的数据进来，共有32路信号
-	datalen = rowdata.shape[1]
-	fftdata = getfft(rowdata)
-
-	freq = map(lambda x: abs(x/datalen),fftdata)
-	freq = map(lambda x: x[: datalen/2+1]*2,freq)
-
 
 	#all frequency signal
 	"""
-	theta:4~7Hz,alpha:8~13Hz,beta:18~30Hz
+	detal:1-4Hz,theta:4~8Hz,alpha:8~13Hz,beta:18~30Hz
 	"""
-	delta = np.arrary(list(map(lambda x: x[datalen*1/fs-1: datalen*4/fs],freq)))
-	theta = np.arrary(list(map(lambda x: x[datalen*4/fs-1: datalen*8/fs],freq)))
-	alpha = np.arrary(list(map(lambda x: x[datalen*8/fs-1: datalen*13/fs],freq)))
-	beta = np.arrary(list(map(lambda x: x[datalen*13/fs-1: datalen*13/fs],freq)))
+	listdata = list(rowdata)
+	delta = np.arrary(list(map(lambda x: butterbandpassfilter(x, 1, 4, fs),listdata)))
+	theta = np.arrary(list(map(lambda x: butterbandpassfilter(x, 4, 8, fs),listdata)))
+	alpha = np.arrary(list(map(lambda x: butterbandpassfilter(x, 8, 13, fs),listdata)))
+	beta = np.arrary(list(map(lambda x: butterbandpassfilter(x, 18, 30, fs),listdata)))
 
 	print(delta.shape)
 
@@ -33,24 +29,74 @@ def getfrequency(rowdata,fs):
 def getfeature(delta,theta,alpha,beta):
 	return delta
 
+def butterbandpass(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+    return b, a
+
+
+def butterbandpassfilter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
 def testfrequency():
-	samplerate = 256
+	samplerate = 512
+
 	t = np.arange(0, 1.0, 1.0/samplerate)
 	x = np.sin(2*np.pi*156.25*t)  + 2*np.sin(2*np.pi*234.375*t)
-	fftx = np.fft.rfft(x)
+	x1 = butterbandpassfilter(x, 100, 200, samplerate)
+	x2 = butterbandpassfilter(x, 200, 250, samplerate)
+
+	fftx = abs(np.fft.rfft(x))
 	freqs = np.linspace(0, samplerate/2,len(fftx))
+
+	fftx1 = abs(np.fft.rfft(x1))
+	fftx2 = abs(np.fft.rfft(x2))
+
 	plt.figure(facecolor='w',figsize=(9,8))
+
+	plt.subplot(211)
 	plt.plot(freqs,fftx,'b-')
+	plt.title('Row Data FFT')
 	plt.grid()
+	plt.subplot(212)
+	plt.plot(t,x,'b-')
+	plt.title('Row Data')
+	plt.show()
+
+	plt.figure(facecolor='w',figsize=(9,8))
+	plt.subplot(211)
+	plt.plot(freqs,fftx1,'b-')
+	plt.title('Row Data FFT')
+	plt.grid()
+	plt.subplot(212)
+	plt.plot(t,x1,'b-')
+	plt.title('Row Data')
+	plt.grid()
+	plt.show()
+
+	plt.figure(facecolor='w',figsize=(9,8))
+	plt.subplot(211)
+	plt.plot(freqs,fftx2,'b-')
+	plt.title('Row Data FFT')
+	plt.grid()
+	plt.subplot(212)
+	plt.plot(t,x2,'b-')
+	plt.title('Row Data')
+	plt.grid()
+
 	plt.show()
 
 	
 	
 if __name__ == '__main__':
-	dataname = 'datas.csv'
-	labelname = 'labels.csv'
+	dataname = 'onedatas.csv'
+	labelname = 'onelabels.csv'
 	fs = 128
-	testfrequency()
-	# data,label = loaddata(dataname, labelname)
-	# getfrequency(data, fs)
+	# testfrequency()
+	data,label = loaddata(dataname, labelname)
+	getfrequency(data, fs)
 

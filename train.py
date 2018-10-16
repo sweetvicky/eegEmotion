21dd352eea889abd48361a93c0a51fb0f004ea33
sweety <sweety.wenjing@gmail.com>
2018-10-16 11:37:27 +0800

    add svmclassier by wenjing

--- train.py
+++ train.py
@@ -2,11 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import butter, lfilter
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt 
+from sklearn import svm
+from sklearn.metrics import accuracy_score
 
 import pyeeg
 
 fs = 128
 tonums = 40 
+frepnums = 4
 
 
 def loaddata(dataname,labelname):
@@ -73,7 +77,11 @@ def getonefeatures(data):
 	# fse = pyeeg.spectral_entropy(data, [0,fs/2], fs)
 	# print(fse)
 
-	featurestmp = np.stack((fstd,fae,fhjormod_com[:,0],fhjormod_com[:,1],fhjor_act),axis=1)
+	# Power Spectral Density
+	fpsd = np.array(list(map(lambda x: np.max(plt.psd(x,fs)[0]),listdata)))
+
+	# Features Stack
+	featurestmp = np.stack((fstd,fae,fhjormod_com[:,0],fhjormod_com[:,1],fhjor_act,fpsd),axis=1)
 	temprow,tmpcol = featurestmp.shape
 	features = np.reshape(featurestmp, (temprow*tmpcol,))
 	return features
@@ -81,33 +89,65 @@ def getonefeatures(data):
 def getfeatures(delta,theta,alpha,beta):
 
     # features's column is setted by yourself
-	features = np.zeros((0,4*5*5))
-	# for r in range(delta.shape[0]):
-	for r in range(1):
+    '''
+    frepnums:numember of frequency ; 
+    channelnums:numember of channel ;
+    featnums:numember of features ;
+    '''
+    channelnums = 2
+    featnums = 6
+    features = np.zeros((0,frepnums*channelnums*featnums))
 
-		tmpdelta = getonefeatures(delta[r,:5,:])
-		tmptheta = getonefeatures(theta[r,:5,:])
-		tmpalpha = getonefeatures(alpha[r,:5,:])
-		tmpbeta = getonefeatures(beta[r,:5,:])
-		tmpfeatures = np.reshape(np.stack((tmpdelta,tmptheta,tmpalpha,tmpbeta),axis=0),(features.shape[1],))
-		# tmpfeatures = np.stack((tmpdelta,tmptheta,tmpalpha,tmpbeta),axis=0)
-		features = np.row_stack((features,tmpfeatures))
+    for r in range(delta.shape[0]):
+        # for r in range(1):
+        tmpdelta = getonefeatures(delta[r,:5,:])
+        tmptheta = getonefeatures(theta[r,:5,:])
+        tmpalpha = getonefeatures(alpha[r,:5,:])
+        tmpbeta = getonefeatures(beta[r,:5,:])
+        tmpfeatures = np.reshape(np.stack((tmpdelta,tmptheta,tmpalpha,tmpbeta),axis=0),(features.shape[1],))
+        # tmpfeatures = np.stack((tmpdelta,tmptheta,tmpalpha,tmpbeta),axis=0)
+        features = np.row_stack((features,tmpfeatures))
 
-	return features
+    return features
 
-def function():
-	pass
+def testpsd(data):
+	fpsd = np.max(plt.psd(data[0,0,:],fs)[0])
+
+def svmclassier(datas,labels):
+    # Split traindata and testdata
+    x_train, x_test, y_train, y_test = train_test_split(datas, labels, train_size=0.8, random_state=1)
+
+    # Classier
+    clf = svm.SVC(C=0.1, kernel='linear', decision_function_shape='ovr')
+    # clf = svm.SVC(C=0.8, kernel='rbf', gamma=20, decision_function_shape='ovr')
+    clf.fit(x_train, y_train.ravel())
+
+    # Accuracy
+    clf.score(x_train,y_train)
+    train_acc = accuracy_score(y_train, clf.predict(x_train))
+
+    clf.score(x_test, y_test)
+    test_acc = accuracy_score(y_test, clf.predict(x_test))
+
+    print("train accurate：%.2f",train_acc)
+    print("test accurate:%.2f",test_acc)
+	
 	
 if __name__ == '__main__':
-	dataname = 'onedatas.csv'
-	labelname = 'onelabels.csv'
+	dataname = '..//data/onedatas.csv'
+	labelname = '..//data/onelabels.csv'
 	fs = 128
-	# testfrequency()
+	# testfrequency()	
 	data,label = loaddata(dataname, labelname)
-	delta,theta,alpha,beta = getfrequency(data)
-	print(delta.shape)
+	indexdata = []
+	for r in range(40):
+		indexdata = indexdata + list(range(r*32,r*32+2))
+	# print(indexdata)
+	delta,theta,alpha,beta = getfrequency(data[indexdata])
+	# print(delta.shape)
 	features = getfeatures(delta,theta,alpha,beta)
-    # x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, random_state=1)
+	# print(features.shape)
+	svmclassier(features, label)
 
 
 
